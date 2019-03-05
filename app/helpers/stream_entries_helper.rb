chikorita157/mastodon-th-sakura

commit 7da087dabe50180cf8e9ce54de9631d3b4ee279e

Store remote votes URI (#10158)
* Store remote votes URI

* Add spec for accepting remote votes

* Make poll vote id generation work the same way as follows

app/models/poll_vote.rb
@@ -9,6 +9,7 @@
 #  choice     :integer          default(0), not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  uri        :string
 #
 
 class PollVote < ApplicationRecord
@@ -19,6 +20,8 @@ class PollVote < ApplicationRecord
   validates_with VoteValidator
 
   after_create_commit :increment_counter_cache
+
+  delegate :local?, to: :account
 
   private
 

app/lib/activitypub/activity/create.rb
@@ -241,7 +241,7 @@ class ActivityPub::Activity::Create < ActivityPub::Activity
 
   def poll_vote?
     return false if replied_to_status.nil? || replied_to_status.poll.nil? || !replied_to_status.local? || !replied_to_status.poll.options.include?(@object['name'])
-    replied_to_status.poll.votes.create!(account: @account, choice: replied_to_status.poll.options.index(@object['name']))
+    replied_to_status.poll.votes.create!(account: @account, choice: replied_to_status.poll.options.index(@object['name']), uri: @object['id'])
   end
 
   def resolve_thread(status)

app/serializers/activitypub/vote_serializer.rb
@@ -6,7 +6,7 @@ class ActivityPub::VoteSerializer < ActiveModel::Serializer
                :in_reply_to, :to
 
     def id
-      [ActivityPub::TagManager.instance.uri_for(object.account), '#votes/', object.id].join
+      ActivityPub::TagManager.instance.uri_for(object) || [ActivityPub::TagManager.instance.uri_for(object.account), '#votes/', object.id].join
     end
 
     def type

db/migrate/20190304152020_add_uri_to_poll_votes.rb
@@ -1,0 +1,5 @@
+class AddUriToPollVotes < ActiveRecord::Migration[5.2]
+  def change
+    add_column :poll_votes, :uri, :string
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_003449) do
+ActiveRecord::Schema.define(version: 2019_03_04_152020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -447,6 +447,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_003449) do
     t.integer "choice", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uri"
     t.index ["account_id"], name: "index_poll_votes_on_account_id"
     t.index ["poll_id"], name: "index_poll_votes_on_poll_id"
   end

spec/lib/activitypub/activity/create_spec.rb
@@ -447,6 +447,27 @@ RSpec.describe ActivityPub::Activity::Create do
           expect(poll.cached_tallies).to eq [10, 3]
         end
       end
+
+      context 'when a vote to a local poll' do
+        let(:poll) { Fabricate(:poll, options: %w(Yellow Blue)) }
+        let!(:local_status) { Fabricate(:status, owned_poll: poll) }
+
+        let(:object_json) do
+          {
+            id: [ActivityPub::TagManager.instance.uri_for(sender), '#bar'].join,
+            type: 'Note',
+            name: 'Yellow',
+            inReplyTo: ActivityPub::TagManager.instance.uri_for(local_status)
+          }
+        end
+
+        it 'adds a vote to the poll with correct uri' do
+          vote = poll.votes.first
+          expect(vote).to_not be_nil
+          expect(vote.uri).to eq object_json[:id]
+          expect(poll.reload.cached_tallies).to eq [1, 0]
+        end
+      end
     end
 
     context 'when sender is followed by local users' do

commit 13218015773ece69432879dc5dd2afee4eb40390

Fix various issues in polls (#10165)
* Fix ActivityPub poll results being serialized even with hide_totals

* Fix poll refresh button having a different font size

* Display poll in OpenGraph description

* Fix NoMethodError when serializing votes

Regression from #10158

* Fix polls on public pages being broken for non-logged-in users

* Do not show time remaining if poll has no expiration date

app/helpers/stream_entries_helper.rb
@@ -104,9 +104,19 @@ module StreamEntriesHelper
     I18n.t('statuses.content_warning', warning: status.spoiler_text)
   end
 
+  def poll_summary(status)
+    return unless status.poll
+    status.poll.options.map { |o| "[ ] #{o}" }.join("\n")
+  end
+
   def status_description(status)
     components = [[media_summary(status), status_text_summary(status)].reject(&:blank?).join(' · ')]
-    components << status.text if status.spoiler_text.blank?
+
+    if status.spoiler_text.blank?
+      components << status.text
+      components << poll_summary(status)
+    end
+
     components.reject(&:blank?).join("\n\n")
   end
 

app/javascript/mastodon/components/poll.js
@@ -118,7 +118,7 @@ class Poll extends ImmutablePureComponent {
           />
 
           {!showResults && <span className={classNames('poll__input', { checkbox: poll.get('multiple'), active })} />}
-          {showResults && <span className='poll__number'>{Math.floor(percent)}%</span>}
+          {showResults && <span className='poll__number'>{Math.round(percent)}%</span>}
 
           {option.get('title')}
         </label>
@@ -146,7 +146,8 @@ class Poll extends ImmutablePureComponent {
         <div className='poll__footer'>
           {!showResults && <button className='button button-secondary' disabled={disabled} onClick={this.handleVote}><FormattedMessage id='poll.vote' defaultMessage='Vote' /></button>}
           {showResults && !this.props.disabled && <span><button className='poll__link' onClick={this.handleRefresh}><FormattedMessage id='poll.refresh' defaultMessage='Refresh' /></button> · </span>}
-          <FormattedMessage id='poll.total_votes' defaultMessage='{count, plural, one {# vote} other {# votes}}' values={{ count: poll.get('votes_count') }} />  · {timeRemaining}
+          <FormattedMessage id='poll.total_votes' defaultMessage='{count, plural, one {# vote} other {# votes}}' values={{ count: poll.get('votes_count') }} />
+          {poll.get('expires_at') && <span> · {timeRemaining}</span>}
         </div>
       </div>
     );

app/javascript/styles/mastodon/polls.scss
@@ -82,6 +82,7 @@
     border: 0;
     color: $dark-text-color;
     text-decoration: underline;
+    font-size: inherit;
 
     &:hover,
     &:focus,

app/models/poll_vote.rb
@@ -23,6 +23,10 @@ class PollVote < ApplicationRecord
 
   delegate :local?, to: :account
 
+  def object_type
+    :vote
+  end
+
   private
 
   def increment_counter_cache

app/serializers/activitypub/note_serializer.rb
@@ -15,8 +15,8 @@ class ActivityPub::NoteSerializer < ActiveModel::Serializer
 
   has_one :replies, serializer: ActivityPub::CollectionSerializer, if: :local?
 
-  has_many :poll_loaded_options, key: :one_of, if: :poll_and_not_multiple?
-  has_many :poll_loaded_options, key: :any_of, if: :poll_and_multiple?
+  has_many :poll_options, key: :one_of, if: :poll_and_not_multiple?
+  has_many :poll_options, key: :any_of, if: :poll_and_multiple?
 
   attribute :end_time, if: :poll_and_expires?
   attribute :closed, if: :poll_and_expired?
@@ -121,8 +121,12 @@ class ActivityPub::NoteSerializer < ActiveModel::Serializer
     object.account.local?
   end
 
-  def poll_loaded_options
-    object.poll.loaded_options
+  def poll_options
+    if !object.expired? && object.hide_totals?
+      object.poll.unloaded_options
+    else
+      object.poll.loaded_options
+    end
   end
 
   def poll_and_multiple?

app/views/stream_entries/_poll.html.haml
@@ -1,5 +1,5 @@
-- options = (!poll.expired? && poll.hide_totals?) ? poll.unloaded_options : poll.loaded_options
-- voted = poll.votes.where(account: current_user.account).exists?
+- options      = (!poll.expired? && poll.hide_totals?) ? poll.unloaded_options : poll.loaded_options
+- voted        = user_signed_in? && poll.votes.where(account: current_account).exists?
 - show_results = voted || poll.expired?
 
 .poll
@@ -9,17 +9,21 @@
         - if show_results
           - percent = 100 * option.votes_count / poll.votes_count
           %span.poll__chart{ style: "width: #{percent}%" }
+
           %label.poll__text><
-            %span.poll__number= percent
+            %span.poll__number= percent.round
             = option.title
         - else
           %label.poll__text><
-            %span.poll__input{ class: poll.multiple ? 'checkbox' : nil}><
+            %span.poll__input{ class: poll.multiple? ? 'checkbox' : nil}><
             = option.title
   .poll__footer
     - unless show_results
       %button.button.button-secondary{ disabled: true }
         = t('statuses.poll.vote')
+
     %span= t('statuses.poll.total_votes', count: poll.votes_count)
-    ·
-    %span= poll.expires_at
+
+    - unless poll.expires_at.nil?
+      ·
+      %span= l poll.expires_at