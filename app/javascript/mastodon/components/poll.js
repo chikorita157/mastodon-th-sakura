chikorita157/mastodon-th-sakura

commit 53f9f8a4cef42df3ad4ee8e02f2724f69871da1a

Fix Poll fetchPoll action not being debounced. (#13485)
* Fix Poll fetchPoll action not being debounced.

* Fix unused import in the Poll component

app/javascript/mastodon/components/poll.js
@@ -4,7 +4,7 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
 import classNames from 'classnames';
-import { vote, fetchPoll } from 'mastodon/actions/polls';
+import { vote } from 'mastodon/actions/polls';
 import Motion from 'mastodon/features/ui/util/optional_motion';
 import spring from 'react-motion/lib/spring';
 import escapeTextContentForBrowser from 'escape-html';
@@ -30,6 +30,7 @@ class Poll extends ImmutablePureComponent {
     intl: PropTypes.object.isRequired,
     dispatch: PropTypes.func,
     disabled: PropTypes.bool,
+    refresh: PropTypes.func,
   };
 
   state = {
@@ -108,7 +109,7 @@ class Poll extends ImmutablePureComponent {
       return;
     }
 
-    this.props.dispatch(fetchPoll(this.props.poll.get('id')));
+    this.props.refresh();
   };
 
   renderOption (option, optionIndex, showResults) {

app/javascript/mastodon/containers/poll_container.js
@@ -1,8 +1,21 @@
 import { connect } from 'react-redux';
+import { debounce } from 'lodash';
+
 import Poll from 'mastodon/components/poll';
+import { fetchPoll } from 'mastodon/actions/polls';
+
+const mapDispatchToProps = (dispatch, { pollId }) => ({
+  refresh: debounce(
+    () => {
+      dispatch(fetchPoll(pollId));
+    },
+    1000,
+    { leading: true },
+  ),
+});
 
 const mapStateToProps = (state, { pollId }) => ({
   poll: state.getIn(['polls', pollId]),
 });
 
-export default connect(mapStateToProps)(Poll);
+export default connect(mapStateToProps, mapDispatchToProps)(Poll);

commit 0f3317fbe2af6727d911d5e5586d3ebdf312a02a

Fix not being able to vote (#13490)
Fix regression introduced by 53f9f8a4cef42df3ad4ee8e02f2724f69871da1a

app/javascript/mastodon/components/poll.js
@@ -4,7 +4,6 @@ import ImmutablePropTypes from 'react-immutable-proptypes';
 import ImmutablePureComponent from 'react-immutable-pure-component';
 import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
 import classNames from 'classnames';
-import { vote } from 'mastodon/actions/polls';
 import Motion from 'mastodon/features/ui/util/optional_motion';
 import spring from 'react-motion/lib/spring';
 import escapeTextContentForBrowser from 'escape-html';
@@ -28,9 +27,9 @@ class Poll extends ImmutablePureComponent {
   static propTypes = {
     poll: ImmutablePropTypes.map,
     intl: PropTypes.object.isRequired,
-    dispatch: PropTypes.func,
     disabled: PropTypes.bool,
     refresh: PropTypes.func,
+    onVote: PropTypes.func,
   };
 
   state = {
@@ -101,7 +100,7 @@ class Poll extends ImmutablePureComponent {
       return;
     }
 
-    this.props.dispatch(vote(this.props.poll.get('id'), Object.keys(this.state.selected)));
+    this.props.onVote(Object.keys(this.state.selected));
   };
 
   handleRefresh = () => {

app/javascript/mastodon/containers/poll_container.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { debounce } from 'lodash';
 
 import Poll from 'mastodon/components/poll';
-import { fetchPoll } from 'mastodon/actions/polls';
+import { fetchPoll, vote } from 'mastodon/actions/polls';
 
 const mapDispatchToProps = (dispatch, { pollId }) => ({
   refresh: debounce(
@@ -12,6 +12,10 @@ const mapDispatchToProps = (dispatch, { pollId }) => ({
     1000,
     { leading: true },
   ),
+
+  onVote (choices) {
+    dispatch(vote(pollId, choices));
+  },
 });
 
 const mapStateToProps = (state, { pollId }) => ({